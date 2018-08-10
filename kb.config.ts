138b486da33881d7bc3492ad05e19febd80d43a1
chenguang <chenguang@agree.com.cn>
2018-08-10 11:00:01 +0800

    修改单元测试脚本。

--- kb.config.ts
+++ kb.config.ts
@@ -1,18 +1,21 @@
 import phoneNumber from './src/phone-number/phone-number.layout';
 
 export default {
-  supplyKeyboardType: {
+  supplyKeyboardType: {//补充键盘布局
     "phoneNumber": {
       layout: phoneNumber,
       keyboardType: "phoneNumber"
     }
   },
-  supplyKey: {
+  supplyKey: { //补充按键
     'keyName': {
       keyName: '',
       action: '',
       value: '',
       event: ""
     }
+  },
+  resetKeyboardLayout: { // 重置键盘布局
+
   }
 }
